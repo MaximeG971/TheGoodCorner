fcba1b70ec60e2df55dc0fd4670a6bd1b7045ada
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-03-05 11:30:23 +0100

    mise en place du useState

--- frontend/src/components/AdCard.tsx
+++ frontend/src/components/AdCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export type AdCardProps = {
   title: string;
   imgUrl: string;
@@ -7,13 +9,13 @@ export type AdCardProps = {
 const AdCard = ({ title, imgUrl, price, link }: AdCardProps) => {
   return (
     <div className="ad-card-container">
-      <a className="ad-card-link" href={link}>
+      <Link className="ad-card-link" href={link}>
         <img className="ad-card-image" alt="ad card image" src={imgUrl} />
         <div className="ad-card-text">
           <div className="ad-card-title">{title}</div>
           <div className="ad-card-price">{price}€</div>
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -1,56 +1,70 @@
+import { useState } from "react";
 import AdCard, { AdCardProps } from "./AdCard";
 
+const ads: AdCardProps[] = [
+  {
+    imgUrl: "/images/table.webp",
+    link: "/ads/table",
+    price: 120,
+    title: "Table",
+  },
+  {
+    imgUrl: "/images/dame-jeanne.webp",
+    link: "/ads/dame-jeanne",
+    price: 75,
+    title: "Dame-Jeanne",
+  },
+  {
+    imgUrl: "/images/vide-poche.webp",
+    link: "/ads/vvide-poche",
+    price: 4,
+    title: "Vide-Poche",
+  },
+  {
+    imgUrl: "/images/vaisselier.webp",
+    link: "/ads/vaisselier",
+    price: 900,
+    title: "Vaisselier",
+  },
+  {
+    imgUrl: "/images/bougie.webp",
+    link: "/ads/bougie",
+    price: 8,
+    title: "Bougie",
+  },
+  {
+    imgUrl: "/images/porte-magazine.webp",
+    link: "/ads/porte-magazine",
+    price: 45,
+    title: "Porte-Magazine",
+  },
+];
+
 const RecentAds = () => {
-  const ads: AdCardProps[] = [
-    {
-      imgUrl: "/images/table.webp",
-      link: "/ads/table",
-      price: 120,
-      title: "Table",
-    },
-    {
-      imgUrl: "/images/dame-jeanne.webp",
-      link: "/ads/dame-jeanne",
-      price: 75,
-      title: "Dame-Jeanne",
-    },
-    {
-      imgUrl: "/images/vide-poche.webp",
-      link: "/ads/vvide-poche",
-      price: 4,
-      title: "Vide-Poche",
-    },
-    {
-      imgUrl: "/images/vaisselier.webp",
-      link: "/ads/vaisselier",
-      price: 900,
-      title: "Vaisselier",
-    },
-    {
-      imgUrl: "/images/bougie.webp",
-      link: "/ads/bougie",
-      price: 8,
-      title: "Bougie",
-    },
-    {
-      imgUrl: "/images/porte-magazine.webp",
-      link: "/ads/porte-magazine",
-      price: 45,
-      title: "Porte-Magazine",
-    },
-  ];
+  const [total, setTotal] = useState(0);
   return (
     <>
       <h2>Annonces récentes</h2>
+      <p>Coût total: {total}€</p>
       <section className="recent-ads">
         {ads.map((ad) => (
-          <AdCard
-            imgUrl={ad.imgUrl}
-            link={ad.link}
-            price={ad.price}
-            title={ad.title}
-            key={ad.title}
-          />
+          <div key={ad.title}>
+            <AdCard
+              imgUrl={ad.imgUrl}
+              link={ad.link}
+              price={ad.price}
+              title={ad.title}
+            />
+            <button
+              className="button"
+              onClick={() => {
+                console.log(ad.price);
+                setTotal(total + ad.price);
+              }}
+            >
+              Ajouter le prix au total
+            </button>
+          </div>
         ))}
       </section>
     </>
